d2de82744c49c2cac9292501d4ac05b4123a6a2a
Ivan Varona <106937432+DeadZerok@users.noreply.github.com>
2025-09-25 23:37:13 -0500

    Create conexiones.h
    configuracion de conexiones red y wifi

---
+++ include/config/conexiones.h
@@ -0,0 +1,13 @@
+// Configuración WiFi
+static const char* WIFI_PASSWORD = "agrega la contraseña de tu red wifi";
+static const char* WIFI_SSID = "agrega el nombre de tu ref wifi";
+
+// Configuración usuario
+static const char* USER_EMAIL = "agrega el correo registrado en la autenticacion de firebase";
+static const char* USER_PASSWORD = "agrega la contraseña registrada en la autenticacion de firebase";
+
+// Configuración Firebase
+static const char* API_KEY = "agrega la api key del proyecto creado en firebase";
+static const char* DATABASE_URL = "agrega la url de la da Realtime Database en firebase";
+
+#endif
